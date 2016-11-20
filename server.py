9eb4c0dc6476c3fe38b1906107fd9582f3688354
Sandra Cobos Sacido <sandra.12@outlook.es>
2016-11-20 13:41:15 +0100

    Empiezo parte servidor, falta aun

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
 """
 Clase (y programa principal) para un servidor de eco en UDP simple
 """
@@ -20,6 +21,8 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     Echo server class
     """
 
+    metodo = line.decode('utf-8').split(' ')[0]
+    METODOS = ['INVITE','BYE','ACK']
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         self.wfile.write(b"Hemos recibido tu peticion")
@@ -31,9 +34,22 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
+            if not metodo in METODOS:
+                self.wfile.write(b'SIP/2.0 405 Method Not Allowed \r\n\r\n')
+            elif metodo = 'INVITE':
+                send = b'SIP/2.0 100 Trying\r\n\r\nSIP/2.0 100 Trying\r\n\r\n'+
+                      +b'SIP/2.0 100 Trying\r\n\r\n'
+                self.wfile.write(send)
+            elif metodo = 'ACK':
+                Ejecuta = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + FICHERO
+                print('Vamos a ejecutar', Ejecuta)
+                os.systema(Ejecuta)
+            elif metodo = 'BYE':
+            else:
+                self.wfile.write(b'SIP/2.0 400 Bad request\r\n\r\n')
 
 if __name__ == "__main__":
-    # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
+    #Lanzamos un servidor SIP
+    serv = socketserver.UDPServer((IP_SERV, PORT_SERV), EchoHandler)
+    print('Listening...')
     serv.serve_forever()
